1b5a81550259ef7e0cc5049f5d79c1c418482a8c
Tegar Bangun Suganda <58646818+suganda8@users.noreply.github.com>
2023-02-16 19:36:44 +0700

    Update: Renaming appName, assets and colors utility class and filename

--- lib/src/utils/app_info.dart
+++ lib/src/utils/app_info.dart
@@ -1,5 +1,5 @@
 class AppInfo {
-  static const appName = 'Ruffle';
+  static const appName = 'SRCDS Tracker';
   static const appVersion = '1.0.0+1';
   static const appVersionName = '1.0.0';
   static const appVersionCode = '1';
--- lib/src/utils/assets.dart
+++ lib/src/utils/assets.dart
@@ -1,23 +1,7 @@
-class A8Assets {
-  static const List<String> assetIcons = [
-    'assets/icons/ic_ko-fi.png',
-  ];
+class AssetsUtility {
+  static const List<String> assetIcons = [];
 
-  static const List<String> assetImages = [
-    'assets/images/img_splash_finch.png',
+  static const List<String> assetImages = [];
 
-    // Figma
-    'assets/images/figma/background_1.png'
-        'assets/images/figma/background_2.png'
-        'assets/images/figma/background_3.png'
-  ];
-
-  static const List<String> assetGraphicImages = [
-    'assets/images/undraw/undraw_moonlight.png',
-    'assets/images/undraw/undraw_notify.png',
-    'assets/images/undraw/undraw_bug_fixing.png',
-    'assets/images/undraw/undraw_image_folder.png',
-    'assets/images/undraw/undraw_not_found.png',
-    'assets/images/undraw/undraw_time_management.png',
-  ];
+  static const List<String> assetGraphicImages = [];
 }
